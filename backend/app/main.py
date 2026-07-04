1a7ed5e19f03616f765f2c4f0656cd23256ab6cd
Lyuba <lyubiksch@gmail.com>
2026-07-04 16:14:52 +0300

    Allow frontend dev port 5501

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,8 @@ app.add_middleware(
         "http://127.0.0.1:8501",
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        "http://localhost:5501",
+        "http://127.0.0.1:5501",
     ],
     allow_credentials=True,
     allow_methods=["*"],
